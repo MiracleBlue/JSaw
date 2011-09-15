6e181950bdba9d73c8ee5c964f734be0880ee628
Nicholas Kircher <miracleblue@miracleblue.com>
2011-09-15 22:10:44 +1000

    Wrote a bunch of complicated crap to test all the other complicated crap :P

--- js/jsaw.js
+++ js/jsaw.js
@@ -10,12 +10,13 @@
  */
 
 // Hackery!  Witchery!  Nonsense and bullshit!
-function construct(constructor, args) {
-	function F() {
-		return constructor.apply(this, args);
+function construct(derpyfunc, args) {
+	//console.log(derpyfunc);
+	function F(derpyfunc) {
+		return derpyfunc.apply(this, args);
 	}
-	F.prototype = constructor.prototype;
-	return new F();
+	F.prototype = derpyfunc.prototype;
+	return new F(derpyfunc);
 }
 
 
@@ -38,7 +39,7 @@ JSAW.Model = {};
 	// Sheduling models
 	JSAW.Model.Schedule = {};
 		// Shedule step model
-		JSAW.Model.Shedule.Step = Backbone.Model.extend({
+		JSAW.Model.Schedule.Step = Backbone.Model.extend({
 			open: true
 		})
 	
@@ -63,13 +64,22 @@ JSAW.Model = {};
 				list: [],
 				// Create synth instance, passing an attribute hash from the related note object to the synth constructor
 				create: function(noteData) {
-					list.push(construct(this.generatorClass, [noteData]));
+					console.log("derp");
+					noteData.audiolet = this.self.al;
+					//console.log(this.self);
+					var voiceObj = construct(this.self.generatorClass, [noteData]);
+					voiceObj.connect(this.self.al.output);
+					this.list.push(voiceObj);
 				}
 			},
 			
 			// Initialize stuff
 			initialize: function(options) {
+				console.log(options);
+				this.al = options.al;
 				this.generatorClass = options.generator;
+				//console.log(this.generatorClass);
+				this.voices.self = this;
 			}
 		});
 	
@@ -154,161 +164,20 @@ JSAW.Model = {};
 /**
  * JSaw global static object
  */
-var JSaw = {
-	version: '0.01',
-	_is_ready: false,
-	bpm: 130,
-	
-	Class: {
-		// Note class
-		Note: Backbone.Model.extend({
-			defaults: {
-				"name": "C",
-				"octave": 1,
-				"velocity": 1.0,
-				"length": 1,
-			},
-			getFullName: function() {
-				return this.get("name") + this.get("octave");
-			}
-		}),
-		
-		// Step class
-		Step: Backbone.Collection.extend({
-			model: JSaw.Class.Note,
-			isBlank: function(){
-				return (this.length < 1);
-			}
-		}),
-		
-		
-	},
-	
-}
 
-/**
- * The awesome JSaw pattern object!
- */
-var JSawPattern = function (params) {
-	
-}
-
-/**
- * The awesome JSaw Piano Roll!
- */
-var PianoRoll = function(params){
-	this.options = {
-		beats: 4,			// Number of beats (length of the piano roll sequence)
-		steps_per_beat: 4,	// Number of steps in a beat (step resolution)
-		bars: 1
-	};
-	
-	// The Step Grid; a horizontally-mapped list of step objects.
-	this.stepGrid = new Array(this.options.steps_per_beat*this.options.beats);
-	
-	$.each(this.stepGrid, function(key, value){
-		//value = new Step();
-		value = new PRStep;
-	});
-	//this.stepGrid = (params.stepGrid ? params.stepGrid : new Array(this.options.steps_per_beat*this.options.beats));
-};
-
-var PRNote = Backbone.Model.extend({
-	defaults: {
-		"name": "C",
-		"octave": 1,
-		"velocity": 1.0,
-		"length": 1,
-	},
-	getFullName: function() {
-		return this.get("name") + this.get("octave");
-	}
-});
-
-// Extensive, descriptive, Note information storage class.
-/*var ExtNote = function(params) {
-	// Setting defaults
-	this.name = 'C';	// Latin name of note
-	this.octave = 0;	// The note octave relative to the base octave
-	this.velocity = 1;	// Velocity of note (volume)
-	
-	this.name = (params.name ? params.name : this.name);
-	this.octave = (params.octave ? params.octave : this.octave);
-	this.velocity = (params.velocity ? params.velocity : this.velocity);
-	
-	// Return note latin name and absolute octave as a string
-	this.noteFromOctave = function(octave) {
-		return this.name + (octave + this.octave);
-	};
-	
-	return this;
-};*/
-
-var PRStep = Backbone.Collection.extend({
-	model: PRNote,
-	isBlank: function(){
-		return (this.length < 1);
-	}
-});
-
-
-// Step class, for an individual step in a sequence
-var Step = function(options) {
-	this.defaults = {
-		notes: [
-			//new ExtNote({name: 'A', octave: 0, velocity: 1}),
-			//new ExtNote({name: 'D', octave: 1, velocity: 1})
-		],
-		position: 0
-	}
-	
-	this.notes = (options.notes ? options.notes : this.defaults.notes);
-	this.position = (options.position ? options.position : this.defaults.position);
-	
-	// Add notes to this step
-	this.addNotes = function(notes) {
-		if (typeof notes.length == "number") {
-			for (i=0; i<notes.length; i++) {
-				this.notes.push(notes[i]);
-			}
-		}
-		
-		
-		return this; // Chainability!
-	};
-	
-	// Remove a particular note object
-	this.removeNote = function(index) {
-		this.notes.remove(index);
-		
-		return this; // Chainability!
-	}
-	
-	// Clears all note objects from this step, effectively making it a blank step
-	this.clearAllNotes = function() {
-		this.notes = [];
-		
-		return this; // Chainability!
-	};
-	
-	// Is this a blank/dead step?
-	this.isDead = function() {
-		if (this.notes.length < 1) return true;
-		return false;
-	};
-	
-	return this;
-};
 
 
 /**
  * Begin initialising application logic here!
  */
 window.onload = function() {
-	var Synth = function(audiolet, frequency) {
+	var Synth = function(params) {
+		var audiolet = params.audiolet;
+		var frequency = params.frequency;
+		
 		AudioletGroup.apply(this, [audiolet, 0, 1]);
 		
-		this.sine = new Saw(this.audiolet, frequency);
+		this.saw = new Saw(this.audiolet, frequency);
 		this.modulator = new Sine(this.audiolet, 2 * frequency);
 		this.modulatorMulAdd = new MulAdd(this.audiolet, frequency / 2, frequency);
 		
@@ -325,9 +194,9 @@ window.onload = function() {
 		);
 		
 		this.modulator.connect(this.modulatorMulAdd);
-		this.modulatorMulAdd.connect(this.sine);
+		this.modulatorMulAdd.connect(this.saw);
 		this.envelope.connect(this.gain, 0, 1);
-		this.sine.connect(this.gain);
+		this.saw.connect(this.gain);
 		this.gain.connect(this.outputs[0]);
 	};
 	extend(Synth, AudioletGroup);
@@ -339,30 +208,7 @@ window.onload = function() {
 		this.octave = 1;
 		
 		var numOfRepeats = 2;
-		/*
-		var stepSequence = [
-			// Beat 1
-			[new ExtNote({name: 'A', octave: 0})],
-			[new ExtNote({name: 'A', octave: 0})],
-			[],
-			[new ExtNote({name: 'A#', octave: 0})],
-			// Beat 2
-			[new ExtNote({name: 'A#', octave: 0})],
-			[],
-			[new ExtNote({name: 'D', octave: 1})],
-			[new ExtNote({name: 'D', octave: 1})],
-			// Beat 3
-			[new ExtNote({name: 'A', octave: 0})],
-			[new ExtNote({name: 'A', octave: 0})],
-			[],
-			[new ExtNote({name: 'A#', octave: 0})],
-			// Beat 4
-			[new ExtNote({name: 'A#', octave: 0})],
-			[],
-			[new ExtNote({name: 'D', octave: 1})],
-			[new ExtNote({name: 'D', octave: 1})]
-		];
-		*/
+		
 		var stepSequence = [
 			// Beat 1
 			[['G',0],['G',1]],
@@ -386,6 +232,8 @@ window.onload = function() {
 			[['G#',1]]
 		];
 		
+		var myInstrument = new JSAW.Model.Instrument.Wrapper({name: "Derpsynth", type: "synth", generator: Synth, al: self.audiolet});
+		
 		// Hooray for overcomplication!
 		for (i=0;i<stepSequence.length;i++) {
 			var noteArr = [];
@@ -393,9 +241,12 @@ window.onload = function() {
 			
 			for (j=0;j<stepSequence[i].length;j++) {
 				//noteArr.push(new ExtNote({name: stepSequence[i][j][0], octave: stepSequence[i][j][1]}));
-				noteArr.push(new PRNote({name: stepSequence[i][j][0], octave: stepSequence[i][j][1]}));
+				//noteArr.push(new PRNote({name: stepSequence[i][j][0], octave: stepSequence[i][j][1]}));
+				noteArr.push({name: stepSequence[i][j][0], octave: stepSequence[i][j][1]});
 			}
-			stepSequence[i] = new PRStep(noteArr);
+			//stepSequence[i] = new PRStep(noteArr);
+			stepSequence[i] = new JSAW.Model.PianoRoll.Step();
+			stepSequence[i].stepRow.add(noteArr);
 		}
 		
 		var frequencyPattern = new PSequence(stepSequence, numOfRepeats);
@@ -408,8 +259,8 @@ window.onload = function() {
 			[frequencyPattern], // Value arrays to iterate over in callback
 			0.25, // Quarter of a beat, 4 steps per beat (0.25)
 			function(step) {
-				if (!step.isDead()){
-					for (i=0;i<step.notes.length;i++){
+				if (!step.isBlank()){
+					/*for (i=0;i<step.notes.length;i++){
 						var nf = Note.fromLatin(step.notes[i].noteFromOctave(this.octave));
 						var synth1 = new Synth(this.audiolet, nf.frequency());
 						var synth2 = new Synth(this.audiolet, (nf.frequency()+3.2));
@@ -417,7 +268,11 @@ window.onload = function() {
 						synth1.connect(this.audiolet.output);
 						synth2.connect(this.audiolet.output);
 						//synth3.connect(this.audiolet.output);
-					}
+					}*/
+					step.stepRow.each(function(note) {
+						var nf = note.getFrequency();
+						myInstrument.voices.create({frequency: nf});
+					})
 				}
 			}.bind(this)
 		);
