0eb7e8bb4fcb62719df64d665965395ee7b03f9d
catshirt <caden@catshirt.net>
2012-07-24 12:43:11 -0400

    last nights commits

--- js/demos/core/mixer.js
+++ js/demos/core/mixer.js
@@ -4,31 +4,31 @@ require([
   'backbone',
   'core/track',
   'core/mixer',
+  'core/scheduler',
   'ui/mixer'
-], function($, _, Backbone, Track, Mixer, MixerView) {
+], function($, _, Backbone, Track, Mixer, Scheduler, MixerView) {
 
   var audiolet = new Audiolet(),
     track = new Track({ audiolet: audiolet }),
     track2 = new Track({ audiolet: audiolet }),
     tracks = new Backbone.Collection([track, track2]),
-    mixer = new Mixer({ audiolet: audiolet, tracks: tracks });
+    mixer = new Mixer({ audiolet: audiolet, tracks: tracks }),
+    scheduler = new Scheduler({ audiolet: audiolet });
 
+  // route graph
   mixer.connect(audiolet.output);
 
-  track.get('instrument').playNotes([
-    { key: 'C' }
-  ]);
+  // repease simple chords
+  scheduler.play([{ key: 'C' }, { key: 'E' }], function(notes) {
+    track.get('instrument').playNotes([notes[0]]);
+    track2.get('instrument').playNotes([notes[1]]);
+  }, 2)
 
-  track2.get('instrument').playNotes([
-    { key: 'E' }
-  ]);
-
+  // build ui
   var mixer_view = new MixerView({
     model: mixer
   });
 
   $('body').append(mixer_view.render().el);
-
-  console.log('instrument played');
 
 });
